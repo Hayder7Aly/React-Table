786d9be8b328c225ffa3a862d68ac94cf3f71bdb
Hayder7Aly <007harryjutt@gmail.com>
2022-01-06 19:20:25 +0500

    learn how to use react table

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+// import BasicTable from './components/BasicTable'
+// import SortingTable from './components/SortingTable'
+// import FilteringTable from "./components/FilteringTable";
+// import PaginationTable from "./components/PaginationTable"
+// import RowSelection from './components/RowSelection'
+// import ColumnOrder from "./components/ColumnOrder";
+// import ColumnHiding from "./components/ColumnHiding";
+import StickyTable from "./components/StickyTable";
+
+import "./App.css";
+
+const App = () => {
+  return (
+    <>
+    <StickyTable />
+    </>
+  );
+};
+
+export default App;
